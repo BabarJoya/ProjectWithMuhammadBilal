92023673680574cbbcc150d391befb02cf2778d9
mbk87234 <48960885+mbk87234@users.noreply.github.com>
2019-04-21 20:32:02 +0500

    Code Scanner
    QR Code Scanner JS Module

---
+++ scan.php
@@ -0,0 +1,28 @@
+﻿<!DOCTYPE html>
+<html>
+<head>
+	<title>GKRSC Code Scanner</title>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<script src="https://rawgit.com/schmich/instascan-builds/master/instascan.min.js"></script>
+</head>
+<body>
+    <h1>GKRSC ID Card Scanner</h1>
+    <video id="preview"></video>
+    <script type="text/javascript">
+      let scanner = new Instascan.Scanner({ video: document.getElementById('preview') });
+      scanner.addListener('scan', function (content) {
+        window.location.href = content;
+      });
+      Instascan.Camera.getCameras().then(function (cameras) {
+        if (cameras.length > 0) {
+          scanner.start(cameras[0]);
+        } else {
+          console.error('No cameras found.');
+        }
+      }).catch(function (e) {
+        console.error(e);
+      });
+
+    </script>
+</body>
+</html>

79ab4711fb2c7a6b69673fa1ebdb38afc8951c29
mbk87234 <48960885+mbk87234@users.noreply.github.com>
2019-04-21 19:30:12 +0500

    Add files via upload

---
+++ p.php
@@ -0,0 +1,261 @@
+﻿<!DOCTYPE HTML>
+<html>
+<head>
+
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css">
+<script type="text/javascript" src="https://www.google.com/jsapi"></script>
+<script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+
+
+<style>
+body {
+  background-color: #ebfaeb;
+}
+.chart {
+  width: 100%; 
+  min-height: 450px;
+}
+.row {
+  margin:0 !important;
+}
+</style>
+
+</head>
+<body>
+<div class="row">
+  <div class="col-md-12 text-center">
+<div style="padding: 10px 0px 0px 0px; margin: 0px;text-align:center;">
+<img class="img-fluid" src="images/templatemo_logo.png"><br><br>
+</div>
+
+<br>
+<br>
+  
+<?php
+
+if(!empty($_GET['i'])) 
+{ $x = $_GET['i']; }
+else
+{ die("<h1>Did not specify any roll number</h1>"); }
+
+
+//$x = '17-1-201';
+
+$x = str_replace("O","1", $x);
+$x = str_replace("T","2", $x);
+$x = str_replace("H","3", $x);
+$x = str_replace("F","4", $x);
+$x = str_replace("I","5", $x);
+$x = str_replace("S","6", $x);
+$x = str_replace("V","7", $x);
+$x = str_replace("E","8", $x);
+$x = str_replace("N","9", $x);
+$x = str_replace("Z","0", $x);
+
+if (file_exists('Data/' . $x . '.txt')){
+	$data = file_get_contents('Data/' . $x . '.txt');
+} else {
+	die("No record found");
+}
+$myObj = json_decode($data, false);
+
+
+echo "<h1>" . $myObj->Stud->nam . " [" . $myObj->Stud->rol . "]</h1>"; 
+
+
+$Rem = "PASS";
+?>
+
+<br>
+<br>
+<table class='table' style='margin: 0 auto'>
+<thead class='thead-dark'>
+	<th>Subject</th>
+	<th>Obtained / Total</th>
+	<th>Remarks</th>
+</thead>
+<tr>
+	<?php
+	echo "<td>" . $myObj->fb->Sub1->nam . "</td>";
+	echo "<td>" . $myObj->fb->Sub1->obt . "/" . $myObj->fb->Sub1->tot . "</td>";
+	if ($myObj->fb->Sub1->per < 33) { echo "<td class='table-danger'>Fail</td>";$Rem="FAIL"; } else { echo "<td class='table-success'>Pass</td>"; }
+	?>
+</tr>
+<tr>
+	<?php
+	echo "<td>" . $myObj->fb->Sub2->nam . "</td>";
+	echo "<td>" . $myObj->fb->Sub2->obt . "/" . $myObj->fb->Sub2->tot . "</td>";
+	if ($myObj->fb->Sub2->per < 33) { echo "<td class='table-danger'>Fail</td>";$Rem="FAIL"; } else { echo "<td class='table-success'>Pass</td>"; }
+	?>
+</tr>
+<tr>
+	<?php
+	echo "<td>" . $myObj->fb->Sub3->nam . "</td>";
+	echo "<td>" . $myObj->fb->Sub3->obt . "/" . $myObj->fb->Sub3->tot . "</td>";
+	if ($myObj->fb->Sub3->per < 33) { echo "<td class='table-danger'>Fail</td>";$Rem="FAIL"; } else { echo "<td class='table-success'>Pass</td>"; }
+	?>
+</tr>
+<tr>
+	<?php
+	echo "<td>" . $myObj->fb->Sub4->nam . "</td>";
+	echo "<td>" . $myObj->fb->Sub4->obt . "/" . $myObj->fb->Sub4->tot . "</td>";
+	if ($myObj->fb->Sub4->per < 33) { echo "<td class='table-danger'>Fail</td>";$Rem="FAIL"; } else { echo "<td class='table-success'>Pass</td>"; }
+	?>
+</tr>
+<tr>
+	<?php
+	echo "<td>" . $myObj->fb->Sub5->nam . "</td>";
+	echo "<td>" . $myObj->fb->Sub5->obt . "/" . $myObj->fb->Sub5->tot . "</td>";
+	if ($myObj->fb->Sub5->per < 33) { echo "<td class='table-danger'>Fail</td>";$Rem="FAIL"; } else { echo "<td class='table-success'>Pass</td>"; }
+	?>
+</tr>
+<tr>
+	<?php
+	echo "<td>" . $myObj->fb->Sub6->nam . "</td>";
+	echo "<td>" . $myObj->fb->Sub6->obt . "/" . $myObj->fb->Sub6->tot . "</td>";
+	if ($myObj->fb->Sub6->per < 33) { echo "<td class='table-danger'>Fail</td>";$Rem="FAIL"; } else { echo "<td class='table-success'>Pass</td>"; }
+	?>
+</tr>
+<tr>
+	<?php
+	if (property_exists($myObj->fb, 'Sub7')) {
+	echo "<td>" . $myObj->fb->Sub7->nam . "</td>";
+	echo "<td>" . $myObj->fb->Sub7->obt . "/" . $myObj->fb->Sub7->tot . "</td>";
+	if ($myObj->fb->Sub7->per < 33) { echo "<td class='table-danger'>Fail</td>";$Rem="FAIL"; } else { echo "<td class='table-success'>Pass</td>"; }
+	}
+	?>
+</tr>
+<tfoot class='thead-light'>
+	<th>Total</th>
+	<?php echo "<th>" . $myObj->fb->Obtained . " / " . $myObj->fb->Total . "</th>"; 
+	if ($Rem == "FAIL") { echo "<td class='bg-danger'>FAIL</td>"; } else { echo "<td class='bg-success'>PASS</td>"; }
+	?>
+</tfoot>
+</table>
+<br>
+
+
+
+
+
+<script type="text/javascript">
+
+google.load("visualization", "1", {packages:["corechart"]});
+google.setOnLoadCallback(drawChart1);
+function drawChart1() {
+  var data = google.visualization.arrayToDataTable([
+          ['Attendance', 'Percent'],
+          ['Present',   <?php echo       $myObj->Stud->att; ?>  ],
+          ['Absent',    <?php echo 100 - $myObj->Stud->att; ?>  ]
+        ]);
+
+  var options = {
+          legend:{position:'none'},
+		  backgroundColor: '#ebfaeb',
+		  colors: ['#2eb82e', '#ff1a1a']
+
+        };
+
+var chart = new google.visualization.PieChart(document.getElementById('chart_div1'));
+  chart.draw(data, options);
+}
+
+
+
+google.setOnLoadCallback(drawChart2);
+function drawChart2() {
+	
+	var data = google.visualization.arrayToDataTable([
+        ['Marks', 'Correct', 'Incorrect', { role: 'annotation' } ],
+		<?php
+		echo  "[ '" . $myObj->fb->Sub1->nam . "'," . $myObj->fb->Sub1->per . "," . (100-$myObj->fb->Sub1->per) . ", ''],";
+        echo  "[ '" . $myObj->fb->Sub2->nam . "'," . $myObj->fb->Sub2->per . "," . (100-$myObj->fb->Sub2->per) . ", ''],";
+		echo  "[ '" . $myObj->fb->Sub3->nam . "'," . $myObj->fb->Sub3->per . "," . (100-$myObj->fb->Sub3->per) . ", ''],";
+		echo  "[ '" . $myObj->fb->Sub4->nam . "'," . $myObj->fb->Sub4->per . "," . (100-$myObj->fb->Sub4->per) . ", ''],";
+		echo  "[ '" . $myObj->fb->Sub5->nam . "'," . $myObj->fb->Sub5->per . "," . (100-$myObj->fb->Sub5->per) . ", ''],";
+		echo  "[ '" . $myObj->fb->Sub6->nam . "'," . $myObj->fb->Sub6->per . "," . (100-$myObj->fb->Sub6->per) . ", '']";
+		if (property_exists($myObj->fb, 'Sub7')) {
+		echo ",[ '" . $myObj->fb->Sub7->nam . "'," . $myObj->fb->Sub7->per . "," . (100-$myObj->fb->Sub7->per) . ", '']";
+		}
+		?>
+
+      ]);
+
+      var options = {
+        bar: { groupWidth: '75%' },
+        isStacked: 'percent',
+        legend:{position:'none'},
+		backgroundColor: '#ebfaeb',
+		colors: ['#2eb82e', '#ff1a1a'],
+		hAxis: {
+            minValue: 0,
+            ticks: [0, .33, .6, .9, 1]
+		}
+      };
+  
+	var options_fullStacked = {
+          isStacked: 'percent',
+          height: 300,
+          legend: {position: 'top', maxLines: 3},
+
+        };
+    
+
+var chart = new google.visualization.BarChart(document.getElementById('chart_div2'));
+  chart.draw(data, options);
+}
+
+
+$(window).resize(function(){
+  drawChart1();
+  drawChart2();
+});
+
+
+
+    </script>
+
+    
+  </div>
+
+  <div class="clearfix"></div>
+  <div class="col-sd-12 col-md-6">
+	<H3 class="text-center">Full Book Exam</h3>
+    <div id="chart_div2" class="chart"></div>
+  </div>
+  <div class="col-sd-12 col-md-6">
+	<h3 class="text-center">Attendance</h3>
+    <div id="chart_div1" class="chart"></div>
+  </div>
+
+</div>
+
+<div class='alert alert-warning text-center'><strong>Remarks: </strong> <?php echo $myObj->Stud->rem; ?> </div>
+
+<div style="padding: 5px 0px 5px 0px; margin: 0px; text-align:center;border-top:1px;text-size:8px;">
+Government Khawaja Rafique Shaheed College, Walton Road, Lahore - <a href="http://www.gkrscollege.edu.pk">http://www.gkrscollege.edu.pk</a>
+</div>
+<footer  class="col-md-12 text-center">This app is in test phase
+<?php
+
+// Add correct path to your countlog.txt file.
+$path = 'Data/countlog.txt';
+
+// Opens countlog.txt to read the number of hits.
+$file  = fopen( $path, 'r' );
+$count = fgets( $file, 1000 );
+fclose( $file );
+
+// Update the count.
+$count = abs( intval( $count ) ) + 1;
+
+// Output the updated count.
+echo  $count ;
+
+// Opens countlog.txt to change new hit number.
+$file = fopen( $path, 'w' );
+fwrite( $file, $count );
+fclose( $file );
+?> </footer>
---
+++ ul.php
@@ -0,0 +1,101 @@
+﻿<?php 
+
+$Key = $_GET["A25"];
+
+if (!isset($Key)) {
+   die("bError: N cName");
+   }
+
+$sSub1 = $_GET["A1"];
+$sSub2 = $_GET["A2"];
+$sSub3 = $_GET["A3"];
+$sSub4 = $_GET["A4"];
+$sSub5 = $_GET["A5"];
+$sSub6 = $_GET["A6"];
+$sSub7 = $_GET["A7"];
+
+$tSub1 = $_GET["A8"];
+$tSub2 = $_GET["A9"];
+$tSub3 = $_GET["A10"];
+$tSub4 = $_GET["A11"];
+$tSub5 = $_GET["A12"];
+$tSub6 = $_GET["A13"];
+$tSub7 = $_GET["A14"];
+
+$oName =  $_GET["A15"];
+$oSub1 =  $_GET["A16"];
+$oSub2 =  $_GET["A17"];
+$oSub3 =  $_GET["A18"];
+$oSub4 =  $_GET["A19"];
+$oSub5 =  $_GET["A20"];
+$oSub6 =  $_GET["A21"];
+$oSub7 =  $_GET["A22"];
+$oAttendance =  $_GET["A23"];
+$oAdmission =   $_GET["A24"];
+$oRoll =        $_GET["A25"];
+
+
+$f = fopen("Data/$Key.txt", 'w') or die("bError:FNF");
+
+$myObj->Stud->nam = $oName;
+$myObj->Stud->rol = $oRoll;
+$myObj->Stud->att = $oAttendance;
+$myObj->Stud->rem = $oAdmission;
+
+if (!$sSub1 == "") {
+$myObj->fb->Sub1->nam = $sSub1;
+$myObj->fb->Sub1->obt = $oSub1;
+$myObj->fb->Sub1->tot = $tSub1;
+if (!$tSub1==0) { $myObj->fb->Sub1->per = ceil(($oSub1/$tSub1)*100); } else { $myObj->fb->Sub1->per = 0;} 
+}
+if (!$sSub2 == "") {
+$myObj->fb->Sub2->nam = $sSub2;
+$myObj->fb->Sub2->obt = $oSub2;
+$myObj->fb->Sub2->tot = $tSub2;
+if (!$tSub2==0) { $myObj->fb->Sub2->per = ceil(($oSub2/$tSub2)*100); } else { $myObj->fb->Sub2->per = 0;} 
+}
+if (!$sSub3 == "") {
+$myObj->fb->Sub3->nam = $sSub3;
+$myObj->fb->Sub3->obt = $oSub3;
+$myObj->fb->Sub3->tot = $tSub3;
+if (!$tSub3==0) { $myObj->fb->Sub3->per = ceil(($oSub3/$tSub3)*100); } else { $myObj->fb->Sub3->per = 0;} 
+}
+if (!$sSub4 == "") {
+$myObj->fb->Sub4->nam = $sSub4;
+$myObj->fb->Sub4->obt = $oSub4;
+$myObj->fb->Sub4->tot = $tSub4;
+if (!$tSub4==0) { $myObj->fb->Sub4->per = ceil(($oSub4/$tSub4)*100); } else { $myObj->fb->Sub4->per = 0;} 
+}
+if (!$sSub5 == "") {
+$myObj->fb->Sub5->nam = $sSub5;
+$myObj->fb->Sub5->obt = $oSub5;
+$myObj->fb->Sub5->tot = $tSub5;
+if (!$tSub5==0) { $myObj->fb->Sub5->per = ceil(($oSub5/$tSub5)*100); } else { $myObj->fb->Sub5->per = 0;} 
+}
+if (!$sSub6 == "") {
+$myObj->fb->Sub6->nam = $sSub6;
+$myObj->fb->Sub6->obt = $oSub6;
+$myObj->fb->Sub6->tot = $tSub6;
+if (!$tSub6==0) { $myObj->fb->Sub6->per = ceil(($oSub6/$tSub6)*100); } else { $myObj->fb->Sub6->per = 0;} 
+}
+if (!$sSub7 == "") {
+$myObj->fb->Sub7->nam = $sSub7;
+$myObj->fb->Sub7->obt = $oSub7;
+$myObj->fb->Sub7->tot = $tSub7;
+if (!$tSub7==0) { $myObj->fb->Sub7->per = ceil(($oSub5/$tSub7)*100); } else { $myObj->fb->Sub7->per = 0;} 
+}
+$myObj->fb->Obtained = $oSub1 + $oSub2 + $oSub3  + $oSub4 + $oSub5 + $oSub6 + $oSub7;
+$myObj->fb->Total    = $tSub1 + $tSub2 + $tSub3  + $tSub4 + $tSub5 + $tSub6 + $tSub7;
+
+$myJSON = json_encode($myObj);
+
+
+
+
+fwrite($f, $myJSON);
+fclose($f);
+
+echo "SENT";
+
+?>
+

8c853adf40629f160e4ac1101c6192ab38d31fbf
BabarJoya <49533864+BabarJoya@users.noreply.github.com>
2019-04-21 22:23:49 +0500

    Add files via upload

--- scan.php
+++ scan.php
@@ -1,12 +1,26 @@
-﻿<!DOCTYPE html>
+<!DOCTYPE html>
 <html>
 <head>
 	<title>GKRSC Code Scanner</title>
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<link rel="stylesheet"  href="bootstrap.min.css">
+  <script src="js/jquery-3.2.1.min.js"></script>
+  <!-- <script src="js/bootstrap.min.js"></script> -->
+  <link rel="stylesheet" href="mystyles.css">
 	<script src="https://rawgit.com/schmich/instascan-builds/master/instascan.min.js"></script>
 </head>
 <body>
-    <h1>GKRSC ID Card Scanner</h1>
+  <div class="container">
+    <div class="row">
+      <div class="col-sm-12 title bg-primary" >
+  <h1 class="text-center ">GKRSC ID Card Scanner</h1>
+      </div>
+
+    <div class="col-sm-12 title bg-info hed2 " >
+  <h3 class="text-center "> Place QR code infront of your camera.</h3>
+      </div>
+     
+    <div class="col-sm-12 box bg-success"  >
+    
     <video id="preview"></video>
     <script type="text/javascript">
       let scanner = new Instascan.Scanner({ video: document.getElementById('preview') });
@@ -22,7 +36,9 @@
       }).catch(function (e) {
         console.error(e);
       });
-
     </script>
+
+    </div>
+  </div>
 </body>
 </html>
